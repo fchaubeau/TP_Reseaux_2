5827e079272690a4eb9cba35830d0a88e4cdff69
fchaubeau <f.chaubeau@gmail.com>
2020-10-20 09:50:25 +0200

    commit

--- src/http/server/WebServer.java
+++ src/http/server/WebServer.java
@@ -7,6 +7,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.nio.file.*;
 
 /**
  * Example program from Chapter 1 Programming Spiders, Bots and Aggregators in
@@ -52,9 +56,27 @@ public class WebServer {
         // blank line signals the end of the client HTTP
         // headers.
         String str = ".";
-        while (str != null && !str.equals(""))
+        String request = "";
+        while (str != null && !str.equals("")){
           str = in.readLine();
+          request += str;
+          request += '\n';
+          //System.out.println(str);
+        }
+        System.out.println(request);
+        String [] splittedRequest = request.split(" ");
+        String method = splittedRequest[0];
+        String path = splittedRequest[1];
 
+        switch(method){
+          case "GET" : 
+            processGetMethod(path, remote, out);
+            break;
+          default :
+          break;
+        }
+
+        /*// Basic response
         // Send the response
         // Send the headers
         out.println("HTTP/1.0 200 OK");
@@ -65,12 +87,60 @@ public class WebServer {
         // Send the HTML page
         out.println("<H1>Welcome to the Ultra Mini-WebServer</H2>");
         out.flush();
+        // End of basic response*/
+
         remote.close();
       } catch (Exception e) {
         System.out.println("Error: " + e);
       }
     }
   }
+
+  public void processGetMethod(String path, Socket client, PrintWriter out){
+    out.println("HTTP/1.0 200 OK");
+    DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    Date currentDate = Calendar.getInstance().getTime();
+    out.println("Date: " + format.format(currentDate));
+    out.println("Content-Type: text/html; charset=UTF-8");
+    out.println("Content-Encoding: UTF-8");
+    // this blank line signals the end of the headers
+    out.println("");
+    // Send the HTML page
+    Path filePath = Paths.get(path);
+    if(Files.exists(filePath)){
+
+    }
+    out.println("<H1>Welcome to the Ultra Mini-WebServer</H2>");
+    out.println("<p>Je l'ai un peu modifie ! </p>");
+    out.flush();
+
+  }
+
+  /*private static void GETRequest(String path, Socket client) throws IOException {
+    if (path.equals("/")) {
+      // Liste toutes les ressources
+      String res = "<h1>Liste des ressources : </h1><ul>";
+      File directoryPath = new File("./http/ressources");
+      String[] files = directoryPath.list();
+      for (String file : files) {
+          res += "<li><a href=\"/" + file + "\">" + file + "</a>" + "</li>";
+      }
+      res += "</ul>";
+      sendResponse(client, "200 OK", "text/html", res.getBytes());
+    }
+    else {
+        Path filePath = getFilePath(path);
+        if (Files.exists(filePath)) {
+            // Le fichier existe
+            String contentType = guessContentType(filePath);
+            sendResponse(client, "200 OK", contentType, Files.readAllBytes(filePath));
+        } else {
+            // Le fichier n'existe pas : 404
+            byte[] notFoundContent = "<h1>Not found :(</h1>".getBytes();
+            sendResponse(client, "404 Not Found", "text/html", notFoundContent);
+        }
+    }
+  }*/
 
   /**
    * Start the application.
